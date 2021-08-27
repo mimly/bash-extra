4ef7c4b1dd2dc648aa2d1c6ed8927831640489d8
mimly <m.j.mlynczak@gmail.com>
2021-08-27 20:31:55 +0200

    Lint bash functions
    Signed-off-by: mimly <m.j.mlynczak@gmail.com>

--- fs4bash.sh
+++ fs4bash.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# shellcheck disable=SC1091
+
 #buildInfo() {
 #    local allCharacters=0
 #    local nonPrintableCharacters=0
@@ -33,21 +35,21 @@
 
 setLocale() {
     local lang=$1
-    if [[ "$lang" -eq "EN" ]] ; then
+    if [[ "${lang}" == "EN" ]] ; then
         localectl set-locale LANG=en_US.utf8 &&\
         localectl --no-convert set-keymap us &&\
         setfont Lat2-Terminus16 &&\
         LANG= &&\
         source /etc/profile.d/locale.sh ;\
         printf "Locales changed [US]\n"
-    elif [[ "$lang" -eq "PL" ]] ; then
+    elif [[ "${lang}" == "PL" ]] ; then
         localectl set-locale LANG=pl_PL.utf8 &&\
         localectl --no-convert set-keymap pl &&\
         setfont Lat2-Terminus16 &&\
         LANG= &&\
         source /etc/profile.d/locale.sh ;\
         printf "Locales changed [PL]\n"
-    elif [[ "$lang" -eq "SV" ]] ; then
+    elif [[ "${lang}" == "SV" ]] ; then
         localectl set-locale LANG=sv_SE.utf8 &&\
         localectl --no-convert set-keymap sv-latin1 &&\
         setfont lat9w-16 &&\
@@ -74,113 +76,102 @@ removeHistoryDuplicates() {
 
 # In order to wrap lines correctly -> checkwinsize/ps
 padWithNonPrintableCharacters() {
-    local allCharacters
+    local allCharacters nonPrintableCharacters output
     read -r -u 7 allCharacters
-    local nonPrintableCharacters
     read -r -u 7 nonPrintableCharacters
 
-    local output=""
+    output=""
     for (( i = 0; i < $(( allCharacters - nonPrintableCharacters )); ++i )) ; do
         output+=""
     done
-    printf "%s" "$output"
+    printf "%s" "${output}"
 }
 
 historyFormatter() {
-    local POSITION_COLOR="[0;38;5;251m"
-    local DATE_COLOR="[0;38;5;253m"
-    local ANCHOR_COLOR=$HOST_DEFAULT_COLOR
-    local COMMAND_COLOR="[0;38;5;255m"
+    local positionColor dateColor anchorColor commandColor
+    positionColor="[0;38;5;251m"
+    dateColor="[0;38;5;253m"
+    anchorColor=${HOST_DEFAULT_COLOR}
+    commandColor="[0;38;5;255m"
 
-    sed -E "s/(\\s+)([[:digit:]]+)(\\s+)(.{19})(\\s+)([#$]{1})(\\s+)(.*)/$ITALIC\1$POSITION_COLOR\2\3$DATE_COLOR\4\5$ANCHOR_COLOR\6\7$COMMAND_COLOR\8$TERMINAL_DEFAULT_COLOR/g"
+    sed -E "s/(\\s+)([[:digit:]]+)(\\s+)(.{19})(\\s+)([#$]{1})(\\s+)(.*)/${ITALIC}\1${positionColor}\2\3${dateColor}\4\5${anchorColor}\6\7${commandColor}\8${TERMINAL_DEFAULT_COLOR}/g"
 }
 
 lsFormatter() {
-    local WHITE="[1;38;5;230m"
-    local YELLOW="[1;38;5;228m"
-    local RED="[1;38;5;204m"
-    local GREEN="[1;38;5;84m"
+    local dirColor rPermColor wPermColor xPermColor inodeColor numberColor uidColor gidColor sizeColor dateColor
+    dirColor="[1;38;5;230m"
+    rPermColor="[1;38;5;228m"
+    wPermColor="[1;38;5;204m"
+    xPermColor="[1;38;5;84m"
+    inodeColor="[1;38;5;112m"
+    numberColor="[1;38;5;113m"
+    uidColor="[1;38;5;114m"
+    gidColor="[1;38;5;115m"
+    sizeColor="[1;38;5;116m"
+    dateColor="[1;38;5;117m"
 
-    local INODE_COLOR="[1;38;5;112m"
-    local NUMBER_COLOR="[1;38;5;113m"
-    local UID_COLOR="[1;38;5;114m"
-    local GID_COLOR="[1;38;5;115m"
-    local SIZE_COLOR="[1;38;5;116m"
-    local DATE_COLOR="[1;38;5;117m"
-
-    sed -E "s/(total [[:digit:]]+(\.[[:digit:]]+)?(K|M|G|T)?)/$SIZE_COLOR\1$RESET/" |
-
-    # Replace r, w and x permission attributes by single rare character.
+    # Replace r, w and x permission attributes by single rare unicode character.
     # ANSI colors include multiple characters.
-    sed -E "s/^([0-9 ]*)d/\1\!/g" |
+    local u1 u2 u3 u4
 
-    sed -E "s/^([0-9 ]*.{1})r/\1\%/g" |
-    sed -E "s/^([0-9 ]*.{4})r/\1\%/g" |
-    sed -E "s/^([0-9 ]*.{7})r/\1\%/g" |
+    u1=香 u2=馘 u3=馗 u4=首 &&\
+        sed -E "s/(total [[:digit:]]+(\.[[:digit:]]+)?(K|M|G|T)?)/${sizeColor}\1${RESET}/" |
 
-    sed -E "s/^([0-9 ]*.{2})w/\1\@/g" |
-    sed -E "s/^([0-9 ]*.{5})w/\1\@/g" |
-    sed -E "s/^([0-9 ]*.{8})w/\1\@/g" |
+        sed -E "s/^([0-9 ]*)d/\1${u1}/g" |
 
-    sed -E "s/^([0-9 ]*.{3})x/\1\?/g" |
-    sed -E "s/^([0-9 ]*.{6})x/\1\?/g" |
-    sed -E "s/^([0-9 ]*.{9})x/\1\?/g" |
+        sed -E "s/^([0-9 ]*.{1})r/\1${u2}/g" |
+        sed -E "s/^([0-9 ]*.{4})r/\1${u2}/g" |
+        sed -E "s/^([0-9 ]*.{7})r/\1${u2}/g" |
 
-    sed -E "s/\!/${WHITE}d$RESET/g" |
-    sed -E "s/\%/${YELLOW}r$RESET/g" |
-    sed -E "s/\@/${RED}w$RESET/g" |
-    sed -E "s/\?/${GREEN}x$RESET/g" |
+        sed -E "s/^([0-9 ]*.{2})w/\1${u3}/g" |
+        sed -E "s/^([0-9 ]*.{5})w/\1${u3}/g" |
+        sed -E "s/^([0-9 ]*.{8})w/\1${u3}/g" |
 
-    sed -E "s/([[:digit:]]+)(\\s+)([[:alpha:]]+)(\\s+)([[:alpha:]]+)(\\s+)([0-9.KMGT]+)(\\s+)((Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec){1}\\s+([[:digit:]]+)\\s+([0-9:]+))/$NUMBER_COLOR\1\2$UID_COLOR\3\4$GID_COLOR\5\6$SIZE_COLOR\7\8$ITALIC$DATE_COLOR\9$RESET/g" |
+        sed -E "s/^([0-9 ]*.{3})x/\1${u4}/g" |
+        sed -E "s/^([0-9 ]*.{6})x/\1${u4}/g" |
+        sed -E "s/^([0-9 ]*.{9})x/\1${u4}/g" |
 
-    sed -E "s/^(\\s*[[:digit:]]+)/$INODE_COLOR\1$RESET/g"
+        sed -E "s/${u1}/${dirColor}d${RESET}/g" |
+        sed -E "s/${u2}/${rPermColor}r${RESET}/g" |
+        sed -E "s/${u3}/${wPermColor}w${RESET}/g" |
+        sed -E "s/${u4}/${xPermColor}x${RESET}/g" |
+
+        sed -E "s/([[:digit:]]+)(\\s+)([[:alpha:]]+)(\\s+)([[:alpha:]]+)(\\s+)([0-9.KMGT]+)(\\s+)((Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec){1}\\s+([[:digit:]]+)\\s+([0-9:]+))/${numberColor}\1\2${uidColor}\3\4${gidColor}\5\6${sizeColor}\7\8${ITALIC}${dateColor}\9${RESET}/g" |
+
+        sed -E "s/^(\\s*[[:digit:]]+)/${inodeColor}\1${RESET}/g"
 }
 
 showDateAndTime() {
-    local output
-    output=$( date | awk '{ printf $0 }' )
-    printf "$YELLOW%s" "$output"
+    printf "${YELLOW}%s" "$( date | awk '{ printf $0 }' )"
 }
 
 showBatteryStatus() {
-    local COLOR
-    local output
-    output=$( acpi | awk '{ gsub( ",", "", $4 ); print $1 ": " $4 }' )
-    local status
+    local color status
     status=$( acpi | awk '{ gsub( /,|%/, "", $4 ); print $4 }' )
-    if [[ $status -lt 10 ]] ; then
-        COLOR=$DARK_RED
-    elif [[ $status -lt 30 ]] ; then
-        COLOR=$RED
-    elif [[ $status -lt 60 ]] ; then
-        COLOR=$YELLOW
+    if [[ ${status} -lt 10 ]] ; then
+        color=${DARK_RED}
+    elif [[ ${status} -lt 30 ]] ; then
+        color=${RED}
+    elif [[ ${status} -lt 60 ]] ; then
+        color=${YELLOW}
     else
-        COLOR=$GREEN
+        color=${GREEN}
     fi
-    printf "$COLOR%s" "$output"
+    printf "${color}%s" "$( acpi | awk '{ gsub( ",", "", $4 ); print $1 ": " $4 }' )"
 }
 
 showEntropy() {
-    local output
-    output=$( awk '{ print "Entropy: " $0 }' < /proc/sys/kernel/random/entropy_avail )
-    printf "$YELLOW%s" "$output"
+    printf "${YELLOW}%s" "$( awk '{ print "Entropy: " $0 }' < /proc/sys/kernel/random/entropy_avail )"
 }
 
 menu() {
-    local dateAndTime
-    dateAndTime=$(showDateAndTime)
-    local batteryStatus
-    batteryStatus=$(showBatteryStatus)
-    local entropy
-    entropy=$(showEntropy)
-
     tput sc &&\
     tput cup 1 98 &&\
-    printf "%s %s %s" "$dateAndTime" "$batteryStatus" "$entropy" &&\
+    printf "%s %s %s" "$(showDateAndTime)" "$(showBatteryStatus)" "$(showEntropy)" &&\
     tput sgr0 &&\
     tput rc
 }
 
 launch() {
-    xinit "$@" -- :0 vt"$XDG_VTNR"
+    xinit "$@" -- :0 vt"${XDG_VTNR}"
 }
